d957e6de46251a281386a31a8ff8fd01de25b222
iziz9 <46iz.us2@gmail.com>
2024-12-06 03:41:42 +0900

    Feat: 내비게이션 바 아이콘 적용 #21

--- src/navigators/members/MainTabNavigator.tsx
+++ src/navigators/members/MainTabNavigator.tsx
@@ -2,6 +2,9 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import MapSharedStack from '../members/sharedStack/MapSharedStack'
 import MyPageSharedStack from './sharedStack/MyPageSharedStack'
 import MatZipSharedStack from './sharedStack/MatZipSharedStack'
+import { View } from 'react-native'
+import { DinoIcon, GalleryIcon, HeartIcon, HomeIcon } from '../../assets/icons/navBarIcon'
+import { COLORS } from '../../constants/variables'
 
 const Tab = createBottomTabNavigator()
 
@@ -19,22 +22,58 @@ function MainTabNavigator() {
         options={{
           headerTitle: '',
           headerShown: false,
+          tabBarActiveTintColor: COLORS.dinosRed,
+          tabBarInactiveTintColor: COLORS.deactivated,
+          tabBarIcon: ({ color, size }) => (
+            <View>
+              <HomeIcon fill={color} />
+            </View>
+          ),
         }}
       />
       <Tab.Screen
-        name="맛집"
+        name="찜"
         component={MatZipSharedStack}
         options={{
           headerTitle: '',
           headerShown: false,
+          tabBarActiveTintColor: COLORS.dinosRed,
+          tabBarInactiveTintColor: COLORS.deactivated,
+          tabBarIcon: ({ color, size }) => (
+            <View>
+              <HeartIcon fill={color} />
+            </View>
+          ),
         }}
       />
       <Tab.Screen
-        name="마이페이지"
+        name="갤러리"
         component={MyPageSharedStack}
         options={{
           headerTitle: '',
           headerShown: false,
+          tabBarActiveTintColor: COLORS.dinosRed,
+          tabBarInactiveTintColor: COLORS.deactivated,
+          tabBarIcon: ({ color, size }) => (
+            <View>
+              <GalleryIcon fill={color} />
+            </View>
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="다이노"
+        component={MyPageSharedStack}
+        options={{
+          headerTitle: '',
+          headerShown: false,
+          tabBarActiveTintColor: COLORS.dinosRed,
+          tabBarInactiveTintColor: COLORS.deactivated,
+          tabBarIcon: ({ color, size }) => (
+            <View>
+              <DinoIcon fill={color} />
+            </View>
+          ),
         }}
       />
     </Tab.Navigator>
