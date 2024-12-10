b642bda0c913762dc85219c750a8978a9ceb6113
iziz9 <46iz.us2@gmail.com>
2024-12-10 16:54:28 +0900

    Feat: 갤러리, 리스트 스크린 준비중 텍스트 추가

--- src/screens/galleryScreen/GalleryScreen.tsx
+++ src/screens/galleryScreen/GalleryScreen.tsx
@@ -1,14 +1,25 @@
-import { Image, SafeAreaView, Text, View } from 'react-native'
-import splash from '../../assets/image/splash.gif'
+import { SafeAreaView, StyleSheet, View } from 'react-native'
+import AppText from '../../components/common/AppText'
+import { DinoIcon } from '../../assets/icons/navBarIcon'
 
 function GalleryScreen() {
   return (
-    <SafeAreaView>
-      <View>
-        <Text>갤러리</Text>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.container}>
+        <DinoIcon fill={'#000'} />
+        <AppText>서비스 준비중입니다.</AppText>
       </View>
     </SafeAreaView>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    gap: 20,
+  },
+})
 
 export default GalleryScreen
--- src/screens/matZipScreens/MatZipListScreen.tsx
+++ src/screens/matZipScreens/MatZipListScreen.tsx
@@ -1,11 +1,25 @@
-import { SafeAreaView, Text } from 'react-native'
+import { SafeAreaView, StyleSheet, View } from 'react-native'
+import { DinoIcon } from '../../assets/icons/navBarIcon'
+import AppText from '../../components/common/AppText'
 
 function MatzipListScreen() {
   return (
-    <SafeAreaView>
-      <Text>맛집리스트</Text>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.container}>
+        <DinoIcon fill={'#000'} />
+        <AppText>서비스 준비중입니다.</AppText>
+      </View>
     </SafeAreaView>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    gap: 20,
+  },
+})
 
 export default MatzipListScreen
