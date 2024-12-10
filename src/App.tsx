90d55936efb6e2a7c7e7eb6d1f04d719b4d98067
iziz9 <46iz.us2@gmail.com>
2024-12-11 01:41:38 +0900

    Feat: IOS gif 스플래시 스크린 적용 #26

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,21 @@
 import { NavigationContainer } from '@react-navigation/native'
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import RootNavigator from './navigators/root/RootNavigator'
 import { QueryClientProvider } from '@tanstack/react-query'
 import queryClient from './libs/react-query'
 import SplashScreen from 'react-native-splash-screen'
+import { Platform } from 'react-native'
+import IosSplashScreen from './screens/common/IosSplashScreen'
 
 function App(): React.JSX.Element {
+  const [isIosLoading, setIsIosLoading] = useState<boolean>(true)
   useEffect(() => {
-    setTimeout(() => {
-      SplashScreen.hide()
-    }, 1000) //스플래시 스크린 숨기기
+    SplashScreen.hide()
   }, [])
 
-  return (
+  return Platform.OS === 'ios' && isIosLoading ? (
+    <IosSplashScreen setIsIosLoading={setIsIosLoading} />
+  ) : (
     <QueryClientProvider client={queryClient}>
       <NavigationContainer>
         <RootNavigator />
