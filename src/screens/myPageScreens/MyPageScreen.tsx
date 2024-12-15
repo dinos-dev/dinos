ddbe5660ce64ed9597b7505025829c4d52998d52
mingyu0711 <minkyu8220@gmail.com>
2024-12-15 17:07:36 +0900

    Chore: yarn.lock 파일 삭제 #28

--- src/screens/myPageScreens/MyPageScreen.tsx
+++ src/screens/myPageScreens/MyPageScreen.tsx
@@ -1,11 +1,14 @@
 import { useNavigation } from '@react-navigation/native'
 import { NativeStackNavigationProp } from '@react-navigation/native-stack'
-import { Button, SafeAreaView, Text } from 'react-native'
+import { Button, SafeAreaView, StyleSheet, Text } from 'react-native'
 import { NaviParams } from '../../constants/NaviParams'
 import { SCREENS } from '../../constants/RoutePath'
 import useAuthStore from '../../store/authStore'
 import { AuthRequest } from '../../services/auth'
 import { UserRequest } from '../../services/user'
+import AppButton from '../../components/common/AppButton'
+import AppText from '../../components/common/AppText'
+import { COLORS } from '../../constants/variables'
 
 function MyPageScreen() {
   const navigation = useNavigation<NativeStackNavigationProp<NaviParams>>()
@@ -20,19 +23,36 @@ function MyPageScreen() {
 
   return (
     <SafeAreaView>
-      <Text style={{ fontSize: 20 }}>마이페이지</Text>
-      <Button title="프로필" onPress={() => navigation.navigate(SCREENS.PROFILE_SCREEN)} />
-      <Button title="회원탈퇴" onPress={() => navigation.navigate(SCREENS.WITHDRAWAL_SCREEN)} />
-      <Button title="로그아웃" onPress={logoutHandler} />
-      <Button
-        title="유저정보"
-        onPress={async () => {
-          const res = await UserRequest.Get.info()
-          console.log('res : ', res)
-        }}
-      />
+      <AppText style={styles.title}>마이페이지</AppText>
+      <AppButton onPress={()=>navigation.navigate(SCREENS.PROFILE_SCREEN)} style={styles.button} textStyle={styles.buttonText}>
+        프로필
+      </AppButton>
+      <AppButton onPress={()=>navigation.navigate(SCREENS.WITHDRAWAL_SCREEN)} style={styles.button} textStyle={styles.buttonText}>
+        회원탈퇴
+      </AppButton>
+      <AppButton onPress={()=>logoutHandler()} style={styles.button} textStyle={styles.buttonText}>
+        로그아웃
+      </AppButton>
     </SafeAreaView>
   )
 }
 
 export default MyPageScreen
+
+const styles = StyleSheet.create({
+  button: {
+    backgroundColor: COLORS.dinosRed,
+    borderRadius: 12,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 5,
+  },
+  buttonText: {
+    color: COLORS.white,
+    fontSize: 16,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  }
+})
